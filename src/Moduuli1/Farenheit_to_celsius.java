521d0e9520295cc0bf76f7a610af67ee2894a3ee
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2024-01-16 14:10:55 +0200

    Fahrenheit to celsius tehtävä tehty

--- src/Moduuli1/Farenheit_to_celsius.java
+++ src/Moduuli1/Farenheit_to_celsius.java
@@ -4,7 +4,7 @@ public class Farenheit_to_celsius {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter tempeature in fahrenheit");
+        System.out.print("Enter temperature in fahrenheit");
         double fahrenheit = scanner.nextDouble();
 
         double celsius = (fahrenheit - 32) * 5 / 9;
