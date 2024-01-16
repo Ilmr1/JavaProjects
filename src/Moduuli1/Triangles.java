2a2a10a6ce39b9d2a4c9794b654e3f52821221bc
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2024-01-16 14:41:49 +0200

    Triangles tehtävä tehty

---
+++ src/Moduuli1/Triangles.java
@@ -0,0 +1,20 @@
+package Moduuli1;
+import java.util.Scanner;
+
+public class Triangles {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of the first leg: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Enter the length of the second leg: ");
+        double b = scanner.nextDouble();
+
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println("The length of the hypotenuse is: " + c);
+    }
+}
+
