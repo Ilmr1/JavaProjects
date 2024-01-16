9805a843ad56a9c3dccdf914c830c3098b84c209
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2024-01-16 14:08:12 +0200

    Fharenheit to celsius task tehty

---
+++ src/Moduuli1/Farenheit_to_celsius.java
@@ -0,0 +1,16 @@
+package Moduuli1;
+import java.util.Scanner;
+public class Farenheit_to_celsius {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter tempeature in fahrenheit");
+        double fahrenheit = scanner.nextDouble();
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.printf("Temperature in celsius: %.1f", celsius);
+
+    }
+}
+
