2e3e588c0a496499dfdf0080181a0b228a9fcc54
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2024-01-21 12:17:41 +0200

    Module 1.4 task 2 valmis

---
+++ Module1/Module1.4/src/Task2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter integer " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        int startIndex = 0, endIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currentSum = 0;
+            for (int j = i; j < n; j++) {
+                currentSum += arr[j];
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    startIndex = i;
+                    endIndex = j;
+                }
+            }
+        }
+
+        System.out.println("Maximum sum: " + maxSum);
+        System.out.println("Integers: " + (startIndex + 1) + "-" + (endIndex + 1));
+    }
+}
