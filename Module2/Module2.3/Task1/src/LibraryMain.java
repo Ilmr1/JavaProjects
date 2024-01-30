642e14f61321739eb43060fd8226fb8db8ddd601
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2024-01-30 12:00:16 +0200

    Module 2.3 Task 1 valmis

--- Module2/Module2.3/Task1/src/LibraryMain.java
+++ Module2/Module2.3/Task1/src/LibraryMain.java
@@ -5,19 +5,15 @@ public class LibraryMain {
         Book book2 = new Book("Data Structures and Algorithms", "Jane Doe", 2018);
         Book book3 = new Book("The Art of Fiction", "Alice Johnson", 2019);
 
-        // Creating a library instance
         Library library = new Library();
 
-        // Adding books to the library
         library.addBook(book1);
         library.addBook(book2);
         library.addBook(book3);
 
-        // Displaying all books in the library
         System.out.println("Library Catalog:");
         library.displayBooks();
 
-        // Finding and displaying books by a specific author
         System.out.println("\nBooks by Author \"Jane Doe\":");
         library.findBooksByAuthor("Jane Doe");
     }
