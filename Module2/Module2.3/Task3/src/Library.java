4f00c7995f699402b3f5fc3d75917c5c3f45657f
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2024-01-31 20:46:01 +0200

    Module 2.3 Task 3 valmis

---
+++ Module2/Module2.3/Task3/src/Library.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Library {
+    private ArrayList<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public void borrowBook(String title) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                iterator.remove();
+                System.out.println("Book borrowed: " + book);
+                return;
+            }
+        }
+        System.out.println("Book not available for borrowing: " + title);
+    }
+
+    public void returnBook(Book book) {
+        books.add(book);
+        System.out.println("Book returned: " + book);
+    }
+
+    // Method to check if a book is available
+    public boolean isBookAvailable(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
