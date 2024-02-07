c02730758f0de95622e2dd8ad758cfc8456d63a6
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2024-02-07 18:08:09 +0200

    Module 3.2.3 valmis

---
+++ Module3/Module3.2/Module3.2.3/src/VehicleDemo.java
@@ -0,0 +1,22 @@
+public class VehicleDemo {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Motorcycle motorcycle = new Motorcycle();
+        Bus bus = new Bus();
+        ElectricCar electricCar = new ElectricCar();
+        ElectricMotorcycle electricMotorcycle = new ElectricMotorcycle();
+
+
+        electricCar.start();
+        electricCar.charge();
+        electricCar.info();
+
+        motorcycle.start();
+        motorcycle.info();
+        motorcycle.stop();
+
+        bus.start();
+        bus.info();
+        bus.stop();
+    }
+}
